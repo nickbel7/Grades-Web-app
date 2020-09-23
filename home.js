fc3498bb88fb101e253e11f01a7db471fe4a2072
nickbel7 <nikolas.bellos@gmail.com>
2020-09-23 18:12:52 +0300

    additions in model
    Signed-off-by: nickbel7 <nikolas.bellos@gmail.com>

--- home.js
+++ home.js
@@ -8,16 +8,38 @@ class Subject {
 
 // Term class: Represents a list of subjects
 class Term {
+	constructor(){
+		this.id = Student.getNextId();
+		this.subjects = [];
+	}
+	/*
 	static getSubjects() {
+		let terms = Student.getTerms();
+		let currentTerm;
+		if (terms == null)
+			return;
+		else {
+			terms.forEach((term, index) => {
+				if (term.id === id) {
+					currentTerm = terms[index];
+				}
+			});
+		}	
 		
+		return currentTerm.subjects;
+	}
+	*/
+	/* mallon axreiasto*/
+	static getSubjects() {
+		return this.subjects;
 	}
 	
-	static addSubject() {
-		
+	static addSubject(subject) {
+		this.subjects.push[subject];
 	}
 	
-	static removeSubject() {
-		
+	static removeSubject(index) {
+		this.subjects.splice(index, 1);
 	}
 }
 
@@ -34,12 +56,28 @@ class Student {
 		return terms;
 	}
 	
-	static addTerm() {
-		
+	static addTerm(term) {
+		const terms = Student.getTerms();
+		terms.push(term);
+		localStorage.setItem('terms', JSON.stringify(terms));
 	}
 	
-	static removeTerm() {
+	static removeTerm(id) {
+		const terms = Student.getTerms();
+		/*
+		terms.forEach((term, index) => {
+			if (term.id === id) {
+				terms.splice(index, 1);
+			}
+		});
+		*/
+		terms.pop();
 		
+		localStorage.setItem('terms', JSON.stringify(terms));
+	}
+	
+	static getNextId() {
+		return Student.getTerms().length;
 	}
 	
 }
